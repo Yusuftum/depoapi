f17dbde9babab67a3ae777245a6f1526fbf9e123
Yusuftum <yusuftum05@gmail.com>
2024-11-18 14:55:14 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const mongoose = require('mongoose');
+const warehouseRoutes = require('./routes/warehouseRoutes');
+const productRoutes = require('./routes/productRoutes');
+const subWarehouseRoutes = require('./routes/subWarehouseRoutes');
+const marketRoutes = require('./routes/marketRoutes');
+const userRoutes = require('./routes/userRoutes');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/connectDB');
+
+const app = express();
+
+require('dotenv').config();
+
+// MongoDB bağlantısı
+connectDB();
+
+// Body parser kullanarak gelen isteklerin JSON olarak işlenmesini sağla
+app.use(bodyParser.json());
+
+// Rotaları kullanma
+app.use('/api/warehouses', warehouseRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/subwarehouses', subWarehouseRoutes);
+app.use('/api/markets', marketRoutes);
+app.use('/api/users', userRoutes); // Kullanıcı rotalarını burada ekledik
+
+// Uygulamanın çalışacağı port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
